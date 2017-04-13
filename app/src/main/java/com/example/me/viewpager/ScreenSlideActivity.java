b83c3fc6319a86ef1e76a5f2750e8560c3ff90da
koshalgarg <koshal.kkg1996@gmail.com>
2017-04-13 18:45:55 +0530

    first commit

---
+++ app/src/main/java/com/example/me/viewpager/ScreenSlideActivity.java
@@ -0,0 +1,13 @@
+package com.example.me.viewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenSlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+    }
+}
